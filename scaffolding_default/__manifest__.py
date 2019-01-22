0fde532b35b815ffe91ab5d9242b63e5843208ad
jobiols <jorge.obiols@gmail.com>
2019-01-22 15:41:26 -0300

    [ADD] Commit inicial

---
+++ scaffolding_default/__manifest__.py
@@ -0,0 +1,69 @@
+# -----------------------------------------------------------------------------
+#
+#    Copyright (C) 2019 NT System Work (http://www.ntsystemwork.com)
+#    All Rights Reserved.
+#
+# -----------------------------------------------------------------------------
+{
+    'name': 'scaffolding',
+    'version': '11.0.0.0.0',
+    'license': 'Other OSI approved licence',
+    'category': 'Tools',
+    'summary': 'Customizacion para scaffolding',
+    'author': 'jeo Software',
+    'depends': [
+        'sale_management',
+        'account_invoicing',
+        'purchase',
+
+        # para la localizacion argentina
+        'l10n_ar_base',  # esto se instala solo
+        'l10n_ar_account',  # esto se instala solo
+        'l10n_ar_afipws_fe',  # Factura Electrónica Argentina
+        'l10n_ar_aeroo_einvoice',  # impresion de factura electronica aeroo
+        'l10n_ar_account_vat_ledger_citi',
+        'account_debt_management',
+        'l10n_ar_aeroo_payment_group',
+
+        # utilitarios
+        'auto_backup',  # poner el backup en: /var/odoo/backups/
+        'backend_theme',
+        'due_payments_argentina_fix'
+    ],
+    'data': [
+    ],
+    'test': [
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'images': [],
+
+    'port': '8069',
+    'repos': [
+        {'usr': 'jobiols', 'repo': 'cl-scaffolding', 'branch': '11.0'},
+        {'usr': 'jobiols', 'repo': 'odoo-addons', 'branch': '11.0'},
+        {'usr': 'jobiols', 'repo': 'rafi16jan-backend-theme',
+         'branch': '11.0'},
+
+        {'usr': 'jobiols', 'repo': 'adhoc-odoo-argentina', 'branch': '11.0'},
+        {'usr': 'jobiols', 'repo': 'adhoc-argentina-sale', 'branch': '11.0'},
+        {'usr': 'jobiols', 'repo': 'adhoc-account-financial-tools',
+         'branch': '11.0'},
+        {'usr': 'jobiols', 'repo': 'adhoc-account-payment', 'branch': '11.0'},
+        {'usr': 'jobiols', 'repo': 'adhoc-miscellaneous', 'branch': '11.0'},
+        {'usr': 'jobiols', 'repo': 'adhoc-argentina-reporting',
+         'branch': '11.0'},
+        {'usr': 'jobiols', 'repo': 'adhoc-reporting-engine', 'branch': '11.0'},
+        {'usr': 'jobiols', 'repo': 'adhoc-aeroo_reports', 'branch': '11.0'},
+        {'usr': 'jobiols', 'repo': 'oca-partner-contact', 'branch': '11.0'},
+        {'usr': 'jobiols', 'repo': 'oca-web', 'branch': '11.0'},
+        {'usr': 'jobiols', 'repo': 'oca-server-tools', 'branch': '11.0'},
+    ],
+    'docker': [
+        {'name': 'odoo', 'usr': 'jobiols', 'img': 'odoo-jeo', 'ver': '11.0'},
+        {'name': 'postgres', 'usr': 'postgres', 'ver': '11.1-alpine'},
+        {'name': 'nginx', 'usr': 'nginx', 'ver': 'latest'},
+        {'name': 'aeroo', 'usr': 'adhoc', 'img': 'aeroo-docs'},
+    ]
+}
