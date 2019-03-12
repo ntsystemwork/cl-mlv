382acf871a807667a50afd88f62d9b68033990db
Jorge Obiols <jobiols@users.noreply.github.com>
2019-03-12 16:08:45 -0300

    Update __manifest__.py

--- scaffolding_default/__manifest__.py
+++ scaffolding_default/__manifest__.py
@@ -61,6 +61,7 @@
         {'usr': 'jobiols', 'repo': 'oca-partner-contact', 'branch': '11.0'},
         {'usr': 'jobiols', 'repo': 'oca-web', 'branch': '11.0'},
         {'usr': 'jobiols', 'repo': 'oca-server-tools', 'branch': '11.0'},
+        {'usr': 'jobiols', 'repo': 'oca-social', 'branch': '11.0'},
     ],
     'docker': [
         {'name': 'odoo', 'usr': 'jobiols', 'img': 'odoo-jeo', 'ver': '11.0'},
